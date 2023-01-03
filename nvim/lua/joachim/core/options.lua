cc511bd9b5e1f6a0076d47ef36e225d9c7d28839
Joachim Birkelund Fredheim <joachibf@stud.ntnu.no>
2023-01-03 17:48:44 +0100

    Added nvim configs

---
+++ nvim/lua/joachim/core/options.lua
@@ -0,0 +1,35 @@
+local opt = vim.opt
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = true
+
+--search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- appeacance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
+opt.iskeyword:append("-")
---
+++ nvim/lua/joachim/plugins/todo-comments.lua
@@ -0,0 +1,4 @@
+local setup, todo_comments = pcall(require, "todo-comments")
+if not setup then
+	return
+end
