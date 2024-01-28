429d853f8cc61e1b29008b29a1166209f9c978c0
Joachim Birkelund Fredheim <joachim@bfredheim.no>
2024-01-28 19:32:26 +0100

    add sketchybar, yabai and skhd

--- .config/nvim/lua/jbfredheim/plugins/catppuccin.lua
+++ .config/nvim/lua/jbfredheim/plugins/catppuccin.lua
@@ -3,7 +3,7 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
-		vim.cmd([[colorscheme catppuccin-latte]])
+		vim.cmd([[colorscheme catppuccin-mocha]])
 		require("catppuccin").setup({
 			color_overrides = {
 				latte = {
--- update.sh
+++ update.sh
@@ -2,10 +2,22 @@
 # Script for updating this repo with current versions of the local files
 
 # nvim
-rsync -av --delete ~/.config/nvim/ ./.config/nvim/
+rsync -av --delete $XDG_CONFIG_HOME/nvim/ ./.config/nvim/
 
 # tmux
 rsync -av --delete ~/.tmux.conf ./.tmux.conf
 
 # zsh
 rsync -av --delete ~/.zshrc ./.zshrc
+
+# alacritty
+rsync -av --delete $XDG_CONFIG_HOME/alacritty/ ./.config/alacritty/
+
+# sketchybar
+rsync -av --delete $XDG_CONFIG_HOME/sketchybar/ ./.config/sketchybar/
+
+# skhd
+rsync -av --delete $XDG_CONFIG_HOME/skhd/ ./.config/skhd/
+
+# yabai
+rsync -av --delete $XDG_CONFIG_HOME/yabai/ ./.config/yabai/
